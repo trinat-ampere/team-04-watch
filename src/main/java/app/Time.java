9a7dcd5a0550b093e961ec7151632309015010e3
pb26 <pasi.bischof@gmail.com>
2016-12-16 12:16:18 +0100

    TimeGenerator created

--- src/main/java/app/Time.java
+++ src/main/java/app/Time.java
@@ -1,10 +1,37 @@
 package app;
 
-public class Time {
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class Time extends Thread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		new Time().start();
 	}
+	
+	@Override
+	public void run() {
+		Clock clock = Clock.system(ZoneId.systemDefault());
+		//System.out.println(clock);
+		while(true) {
+			//System.out.println(clock.instant());
+			long timeMS = System.currentTimeMillis();
+			System.out.println(timeMS);
+			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+			Date resultdate = new Date(timeMS);
+			String dateString = sdf.format(resultdate);
+			System.out.println(dateString);
+		try {
+			Thread.sleep(997);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	}
+
+	
 
 }
